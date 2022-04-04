bdf6d68ca7a5b43ffc8f5e8a5762eb42338d7886
SimonRusu <simonrust97@gmail.com>
2022-04-04 12:29:52 +0100

    create timetable component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
+import { TimetableComponent } from './timetable/timetable.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    TimetableComponent
   ],
   imports: [
     BrowserModule,
