690b08c970f2bd994bba5db0750ce93ea5a824ea
SimonRusu <simonrust97@gmail.com>
2022-04-05 09:42:52 +0100

    added route-settings component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouteSettingsComponent } from './route-settings/route-settings.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    RouteSettingsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/route-settings/route-settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-route-settings',
+  templateUrl: './route-settings.component.html',
+  styleUrls: ['./route-settings.component.css']
+})
+export class RouteSettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
