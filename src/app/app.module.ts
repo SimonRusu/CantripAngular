9d8ac9b581ee35f407a16cad270b543e5bb20d31
RaulMedR <raul.medina106@alu.ulpgc.es>
2022-04-13 20:55:57 +0100

    Obtained JSON data and visualized in predefined-routes-page.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HeaderComponent } from './header/header.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {HttpClientModule} from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
--- src/app/route.service.ts
+++ src/app/route.service.ts
@@ -7,10 +7,11 @@ import { HttpClient } from "@angular/common/http";
 export class RouteService {
 
   constructor(private http: HttpClient) { }
-
+  //predefinedRoutesList !: Array<Object>;
   getPredefinedRoutes(){
     return this.http.get<{routeId: number, routeName: string, numberOfPeople: number, price: number,
       thumbnailPath: string, dateAvailability: string[], publishDate: string, activities: string[],
       startTime: string}[]>('/assets/json/predefinedRoutes.json');
   }
+
 }
