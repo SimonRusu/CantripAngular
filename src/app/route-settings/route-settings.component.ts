f6edebe8997021dc41c4042c4838546e7dd1d051
María Naranjo Almeida <maria2000naranjo@gmail.com>
2022-04-19 09:01:22 +0100

    Adding data in route-settings from JSON

--- src/app/route-settings/route-settings.component.ts
+++ src/app/route-settings/route-settings.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-route-settings',
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./route-settings.component.css']
 })
 export class RouteSettingsComponent implements OnInit {
-
+  @Input() numberOfPeople !: number;
+  @Input() date !: string;
+  @Input() startingTime !: string;
   constructor() { }
 
   ngOnInit(): void {
