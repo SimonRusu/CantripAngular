9997326f9dfb68b2d072396787995975757bbdfd
Simon Rusu <simon.rusu101@alu.ulpgc.es>
2022-05-02 21:11:05 +0100

    get url from imageUpload

--- src/services/file-upload.service.ts
+++ src/services/file-upload.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/compat/database';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { finalize } from 'rxjs/operators';
 import { FileUpload } from '../models/fileUpload';
 @Injectable({
@@ -10,24 +10,25 @@ import { FileUpload } from '../models/fileUpload';
 export class FileUploadService {
   private basePath = '/users';
   constructor(private db: AngularFireDatabase, private storage: AngularFireStorage) { }
-  pushFileToStorage(fileUpload: FileUpload): Observable<number | undefined> {
+  
+  pushFileToStorage(fileUpload: FileUpload): Observable<string> {
+    var subject = new Subject<string>();
     const filePath = `${this.basePath}/${fileUpload.file.name}`;
     const storageRef = this.storage.ref(filePath);
     const uploadTask = this.storage.upload(filePath, fileUpload.file);
+
     uploadTask.snapshotChanges().pipe(
       finalize(() => {
         storageRef.getDownloadURL().subscribe(downloadURL => {
-          fileUpload.url = downloadURL;
-          fileUpload.name = fileUpload.file.name;
-          this.saveFileData(fileUpload);
+          subject.next(downloadURL);
         });
       })
     ).subscribe();
-    return uploadTask.percentageChanges();
+    return subject.asObservable();
   }
-  private saveFileData(fileUpload: FileUpload): void {
-    this.db.list(this.basePath).push(fileUpload);
-  }
+
+
+
   getFiles(numberItems: number): AngularFireList<FileUpload> {
     return this.db.list(this.basePath, ref =>
       ref.limitToLast(numberItems));
