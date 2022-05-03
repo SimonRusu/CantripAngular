88ba8c80e7d7016bcb7e4578736b0fbb4fcdc025
María Naranjo Almeida <maria2000naranjo@gmail.com>
2022-05-03 10:33:56 +0100

    Added sign and check email functionality

---
+++ src/services/firestore/fire-auth.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FireAuthService {
+  userData: Observable<any>;
+
+  constructor(private angularFireAuth: AngularFireAuth) {
+  }
+
+  async singUp(email: string, password: string) {
+
+    this.angularFireAuth.createUserWithEmailAndPassword(email, password).then(resp => {
+      console.log("User succesfully signed up");
+
+    })
+      .catch(error => {
+        console.log("Something went wrong in register: ", error.message);
+
+      })
+
+  }
+
+  async singIn(email: string, password: string): Promise<boolean> {
+    let succesfully !: boolean;
+    console.log(succesfully);
+    await this.angularFireAuth.signInWithEmailAndPassword(email, password).then(() => {
+      console.log("User succesfully login");
+      succesfully = true;
+    })
+      .catch((error) => {
+        console.log("Something went wrong in login: ", error.message);
+        succesfully = false;
+      })
+    return succesfully;
+  }
+
+  SignOut() {
+    this.angularFireAuth.signOut();
+  }
+
+  async checkEmailExists(email: string): Promise<boolean> {
+    let exists !: boolean;
+    await this.angularFireAuth.fetchSignInMethodsForEmail(email).then(user => {
+      if (user.length > 0) {
+        console.log("User exists");
+        exists = true;
+      } else {
+        console.log("User doesn't exist");
+        exists = false;
+      }
+    })
+    return exists;
+  }
+}
