a91302c3b953168c0438844462f19fbb3c258e0c
Simon Rusu <simon.rusu101@alu.ulpgc.es>
2022-05-07 18:48:46 +0100

    file-upload refactor

--- src/services/file-upload.service.ts
+++ src/services/file-upload.service.ts
@@ -32,25 +32,4 @@ export class FileUploadService {
       })
     ).subscribe();
   }
-
-
-
-  getFiles(numberItems: number): AngularFireList<FileUpload> {
-    return this.db.list(this.basePath, ref =>
-      ref.limitToLast(numberItems));
-  }
-  deleteFile(fileUpload: FileUpload): void {
-    this.deleteFileDatabase(fileUpload.key)
-      .then(() => {
-        this.deleteFileStorage(fileUpload.name);
-      })
-      .catch(error => console.log(error));
-  }
-  private deleteFileDatabase(key: string): Promise<void> {
-    return this.db.list(this.basePath).remove(key);
-  }
-  private deleteFileStorage(name: string): void {
-    const storageRef = this.storage.ref(this.basePath);
-    storageRef.child(name).delete();
-  }
 }
